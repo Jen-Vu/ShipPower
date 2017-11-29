5eb6704a490467a7746cd303cb1a2b0764772e2c
DaiPei <daipei@hust.edu.cn>
2017-11-29 13:13:20 +0800

    init project

---
+++ ShipPower/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ShipPower
+//
+//  Created by DaiPei on 2017/10/31.
+//  Copyright © 2017年 DaiPei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
