fab254f593469bb3e1b14f5a24c79c7d493a441e
DaiPei <daipei@hust.edu.cn>
2017-11-29 13:16:35 +0800

    add SPEditView.swift

---
+++ ShipPower/SPEditView.swift
@@ -0,0 +1,17 @@
+//
+//  SPEditView.swift
+//  ShipPower
+//
+//  Created by DaiPei on 2017/11/1.
+//  Copyright © 2017年 DaiPei. All rights reserved.
+//
+
+import Cocoa
+
+class SPEditView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+    }
+    
+}
