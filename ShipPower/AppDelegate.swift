138109c977691a182e3408a32268aef68b5478f1
DaiPei <daipei@hust.edu.cn>
2017-11-29 15:01:44 +0800

    make application respond to icon click

--- ShipPower/AppDelegate.swift
+++ ShipPower/AppDelegate.swift
@@ -11,14 +11,25 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var window: NSWindow?
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        window = NSApplication.shared.mainWindow
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if flag {
+            return false
+        } else {
+            window?.makeKeyAndOrderFront(self)
+            return true
+        }
     }
 
 }
